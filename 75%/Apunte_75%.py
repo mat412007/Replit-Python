0808511654b1090b89e5d860f9946ed8b7f6e11c
Appaz <matias2007play4@gmail.com>
2024-02-14 01:50:11 -0300

    Update

--- 75%/Apunte_75%.py
+++ 75%/Apunte_75%.py
@@ -580,3 +580,313 @@ while True:
     addEntry()
   else:
     viewEntry()
+  answer = input("Wanna go again? ")
+  if answer.strip().lower()[0] == "y":
+    continue
+  else:
+    break
+    
+# Multiple Files
+"""By now, you have written some pretty big programs with lots of lines of code. This can get pretty cumbersome to deal with. Lots of scrolling to find the right bit... One of the ways to overcome this is to split the code into multiple files."""
+import test as tt 
+print("Countdown")
+tt.countdown() # Test refers to the file, countdown to the subroutine in that file.
+
+# OOP
+"""Object Oriented Programming (OOP) is a programming paradigm (a way of thinking about how to solve a problem) that is based on classes and objects, which store all of their data and behaviors inside them.
+You can think of a class like a cookie cutter, or template. It has pre-defined characteristics (shape, size etc).
+Instantiation means 'use the template to create an object'. Like pressing the cutter into the dough to make a cookie."""
+class animal:
+  species = None
+  name = None
+  sound = None
+  # Sets the characteristics
+  # El nombre __init__ significa que esté código será recorrido tan pronto la clase esté incluida
+
+  def __init__(self, name, species, sound):# self está siempre porque se refiere al objeto en sí, no a una variable
+    self.name = name
+    self.species = species
+    self.sound = sound
+  # Constructor used to set the object parameters
+  def talk(self):
+    print((f"{self.name} says {self.sound}")) 
+  # 'self' means 'use the identifier given to the object that is accessing this method'. So If I use it with dog it will become 'dog.talk()' etc.
+
+class bird(animal):
+  def __init__(self, color):
+    self.name = "Bird"
+    self.species = "Avian"
+    self.sound = "Tweet"
+    self.color = color # Only applies to the bird sub class
+
+    # This automatically sets the information for each bird when it is created.
+polly = bird("Green") # Sets polly's colour to 'Green'
+polly.talk() # polly uses the `talk()` method from the animal class
+print(polly.color) # Prints polly's color
+dog = animal("Brian", "Canine", "Woof")
+print(dog.name)
+cow = animal("Ermintrude", "Bo Taurus", "Moo")
+print(cow.sound)
+"""Subrutinas dentro de objetos se llaman métodos"""
+dog = animal("Brian", "Canine", "Woof")
+dog.talk()
+cow = animal("Ermintrude", "Bo Taurus", "Moo")
+cow.talk()
+"""------------------------------"""
+class job:
+  name = None
+  salary = None
+  hoursWorked = None
+
+  def __init__(self, name, salary, hoursWorked):
+    self.name = name
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+
+  def print(self):
+    print("== JOB ==")
+    print()
+    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
+
+class doctor(job):
+  experience = None
+  speciality = None
+
+  def __init__(self, salary, hoursWorked, experience, speciality):
+    self.name = "Doctor"
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+    self.experience = experience
+    self.speciality = speciality
+
+  def print(self):
+    print("== JOB ==")
+    print()
+    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
+    print(f"{self.experience:<10} {self.speciality:>21}")
+
+class teacher(job):
+  subject = None
+  position = None
+
+  def __init__(self, salary, hoursWorked, subject,  position):
+    self.name = "Teacher"
+    self.hoursWorked = hoursWorked
+    self.salary = salary
+    self.subject = subject
+    self.position = position
+  
+  def print(self):
+    print("== JOB ==")
+    print()
+    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
+    print(f"{self.subject:<10} {self.position:>21}")
+
+lawyer = job("Lawyer", "$100,000", "40")
+lawyer.print()
+doc = doctor("$120,000", "48", "7", "Pediatric Consultant")
+doc.print()
+teach = teacher("$50,000", "48+", "CompSci", "Asst. Principal")
+teach.print()
+
+# Character Creation
+class character:
+  name = None
+  health = 100
+  mp = 100
+
+  def __init__(self, name):
+    self.name = name
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}")
+
+  def setStats(self, health, mp):
+    self.health = health
+    self.mp = mp
+
+class player(character):
+  nickname = None
+  lives = 3
+
+  def __init__(self, nickname):
+    self.name = "Player"
+    self.nickname = nickname
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}\tNickname: {self.nickname}\tLives: {self.lives}")
+
+  def isAlive(self):
+    if self.lives > 0:
+      print(f"{self.nickname} lives on!")
+      return True
+    else:
+      print(f"{self.nickname} has expired!")
+      return False
+
+ian = player("Ian the mighty")
+ian.print()
+print(ian.isAlive())
+
+class enemy(character):
+  type = None
+  strength = None
+
+  def __init__(self, name, type, strength):
+    self.name = name
+    self.type = type
+    self.strength = strength
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}\tType: {self.type}\tStrength: {self.strength}")
+
+class orc(enemy):
+  speed = None
+
+  def __init__(self, speed):
+    self.name = "Orc"
+    self.type = "Orc"
+    self.strength = 200
+    self.speed = speed
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}\tType: {self.type}\tStrength: {self.strength}\tSpeed: {self.speed}")
+
+sharron = orc(250)
+gary = orc(205)
+
+sharron.print()
+gary.print()
+
+class vampire(enemy):
+  day = True
+
+  def __init__(self, day):
+    self.name = "Vampire"
+    self.type = "Vampire"
+    self.strength = 150
+    self.day = day
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}\tType: {self.type}\tStrength: {self.strength}\tDay: {self.day}")
+
+eric = vampire(False)
+eric.print()
+
+# GUI (Graphical User Interface)
+"""tkinter is one of the most popular Python GUI libraries."""
+import tkinter as tk
+
+def updateLabel():
+  global label # Uses the values in the label variable
+  label += 1 # Adds one to the value in the label
+  hello["text"] = label 
+  # Subroutine that updates the text in the label.
+
+window = tk.Tk()
+window.title("Hello World") # Sets the name of the window in the border
+window.geometry("300x300") # Sets the size of the window in pixels
+
+label = "Hey there world"
+hello = tk.Label(text = label) # Creates a text box
+hello.pack() # 'pack' places the element on the screen
+
+label = 0
+button = tk.Button(text = "Click me!", command = updateLabel) # Calls the updateLabel subroutine when the button is clicked
+button.pack()
+
+text = tk.Text(window ,height=1, width = 40)
+# Three arguments, name of the window to place the text box in, height & width.
+text.pack()
+tk.mainloop()
+"""----------------------------------"""
+window = tk.Tk()
+window.title("Calculator")
+window.geometry("400x200")
+
+answer = 0
+lastNumber = 0
+operator = None
+
+def typeAnswer(value):
+  global answer
+  answer = f"{answer}{value}"
+  answer = int(answer)
+  hello["text"] = answer
+
+def calcAnswer(thisOp):
+  global answer, lastNumber, operator
+  lastNumber = answer
+  answer = 0
+  if thisOp == "+":
+    operator = "+"
+  elif thisOp == "-":
+    operator = "-"
+  elif thisOp == "*":
+    operator = "*"
+  elif thisOp == "/":
+    operator = "/"
+  hello["text"] = answer
+
+def calc():
+  global answer, lastNumber, operator
+  print(f"{lastNumber}\t{answer}\t{operator}")
+  if operator =="+":
+    total = lastNumber + answer
+  elif operator =="-":
+    total = lastNumber - answer
+  elif operator =="*":
+    total = lastNumber * answer
+  elif operator =="/":
+    total = lastNumber / answer
+  answer = total
+  hello["text"] = answer
+
+hello = tk.Label(text=answer)
+hello.grid(row=0, column=1)
+
+one = tk.Button(text="1", command= lambda: typeAnswer(1))
+one.grid(row=1,column=0)
+
+two = tk.Button(text="2", command= lambda: typeAnswer(2))
+two.grid(row=1,column=1)
+
+three = tk.Button(text="3", command= lambda: typeAnswer(3))
+three.grid(row=1,column=2)
+
+four = tk.Button(text="4", command= lambda: typeAnswer(4))
+four.grid(row=2,column=0)
+
+five = tk.Button(text="5", command= lambda: typeAnswer(5))
+five.grid(row=2,column=1)
+
+six = tk.Button(text="6", command= lambda: typeAnswer(6))
+six.grid(row=2,column=2)
+
+seven = tk.Button(text="7", command= lambda: typeAnswer(7))
+seven.grid(row=3,column=0)
+
+eight = tk.Button(text="8", command= lambda: typeAnswer(8))
+eight.grid(row=3,column=1)
+
+nine = tk.Button(text="9", command= lambda: typeAnswer(9))
+nine.grid(row=3,column=2)
+
+zero = tk.Button(text="0", command= lambda: typeAnswer(0))
+zero.grid(row=4,column=1)
+
+add = tk.Button(text="+", command= lambda: calcAnswer("+"))
+add.grid(row=1,column=4)
+
+minus = tk.Button(text="-", command= lambda: calcAnswer("-"))
+minus.grid(row=1,column=5)
+
+multiply = tk.Button(text="*", command= lambda: calcAnswer("*"))
+multiply.grid(row=2,column=4)
+
+divide = tk.Button(text="/", command= lambda: calcAnswer("/"))
+divide.grid(row=2,column=5)
+
+equals = tk.Button(text="=", command=calc)
+equals.grid(row=4,column=4)
+tk.mainloop()
